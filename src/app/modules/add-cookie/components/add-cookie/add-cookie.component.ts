9a2ca0a1e371496fd6e5a4947846ab4f9b8e9ccd
Carles Company Soler <carlescs@gmail.com>
2023-09-17 15:51:28 +0200

    Add first version of add-cookie

--- src/app/modules/add-cookie/components/add-cookie/add-cookie.component.ts
+++ src/app/modules/add-cookie/components/add-cookie/add-cookie.component.ts
@@ -1,9 +1,26 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AddCookieService } from '../../services/add-cookie.service';
 
 @Component({
   selector: 'app-add-cookie',
   standalone: true,
+  imports:[ReactiveFormsModule],
   templateUrl: './add-cookie.component.html',
   styleUrls: ['./add-cookie.component.scss'],
 })
-export class AddCookieComponent {}
+export class AddCookieComponent {
+  public formgroup:FormGroup;
+
+  constructor(private _addCookieService: AddCookieService, fb: FormBuilder) { 
+    this.formgroup = fb.group({
+      name: new FormControl('', [Validators.required]),
+      value: new FormControl('', [Validators.required])
+    });
+  }
+
+  public save(){
+    this._addCookieService.AddCookie(this.formgroup.value.name, this.formgroup.value.value)
+    .subscribe(()=>alert("OK"));
+  }
+}
