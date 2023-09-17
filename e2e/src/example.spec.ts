d4a84bdd86dd9489e9e998641fdb0c27adf16ead
Carles Company Soler <carlescs@gmail.com>
2023-09-17 10:17:18 +0200

    Remove example component

--- e2e/src/example.spec.ts
+++ e2e/src/example.spec.ts
@@ -2,7 +2,6 @@ import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
   await page.goto('/');
-
-  // Expect h1 to contain a substring.
-  expect(await page.locator('h1').innerText()).toContain('Welcome');
+  const title = page.locator('h1');
+  await expect(title).toHaveText('Test Cookies');
 });
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,27 +1,17 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
 import { RouterTestingModule } from '@angular/router/testing';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent, NxWelcomeComponent, RouterTestingModule],
+      imports: [AppComponent, RouterTestingModule],
     }).compileComponents();
   });
 
-  it('should render title', () => {
+  it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain(
-      'Welcome test-cookies-front'
-    );
-  });
-
-  it(`should have as title 'test-cookies-front'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('test-cookies-front');
+    const app = fixture.componentInstance as AppComponent;
+    expect(app).toBeTruthy();
   });
 });
